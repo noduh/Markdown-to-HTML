22c49c4d2ba368149a332e4958b57a87f906ddaf
Noah Jorgensen <njorgensen@byui.edu>
2026-04-29 19:45:27 -0600

    created functionality for reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+use std::io;
+
 fn main() {
-    println!("Hello, World!")
+    let mut file_path = String::new();
+
+    println!("Please give the file path: ");
+    io::stdin()
+        .read_line(&mut file_path)
+        .expect("Failed to read line");
+
+    let file_contents = fs::read_to_string(file_path.trim()).expect("Failed to read the file");
+
+    println!("The file reads:\n\n{file_contents}");
 }
