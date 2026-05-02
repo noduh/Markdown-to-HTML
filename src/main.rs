42c0b25c9b72ff92534aa1caf9acfdcde34bbcb4
Noah Jorgensen <njorgensen@byui.edu>
2026-05-01 18:42:23 -0600

    BROKEN: New data structure. Work in progress.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::io;
 
@@ -19,4 +20,27 @@ fn file_to_string(file_path: &str) -> String {
     let file_contents = fs::read_to_string(file_path.trim()).expect("Failed to read the file");
 
     file_contents
+}
+
+// Nested Map Data Structure
+pub enum NestedMapNodeData<'a> {
+    Value(String),
+    Map(HashMap<String, NestedMap<'a>>),
+}
+pub struct NestedMap<'a> {
+    data: NestedMapNodeData<'a>,
+    parent_node: Option<&'a HashMap<String, NestedMapNodeData<'a>>>, // if it has a parent, the parent is a map
+    child_node: Option<&'a NestedMapNodeData<'a>>,                   // a child could be anything
+}
+impl<'a> NestedMap<'a> {
+    pub fn new() -> Self {
+        Self {
+            data: , // I DON'T KNOW YET WHAT TO PUT HERE
+            parent_node: None, // it is the root, therefore no parent
+            child_node: None,
+        }
+    }
+    pub fn insert(&mut self, key: &str, value: NestedMapNodeData) {
+        self.root_node.insert(key.to_string(), value);
+    }
 }
